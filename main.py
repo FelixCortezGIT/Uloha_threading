2c85d7e5f4da2561c75ec16ec3f6f9798e4d7e42
TomasMacula <tomasmacula@live.com>
2026-03-09 10:01:18 +0100

    pridane funkcie find_max a find_min

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import threading
 user = input("zadaj cisla oddelene medzerou: ")
 numbers = list(map(int, user.split()))
 
+def find_max():
+    maximum = max(numbers)
+    print("najvyssie cislo: ", maximum)
+
+def find_min():
+    minimum = min(numbers)
+    print("najnizsie cislo: ", minimum)
+
+
+
 print(numbers)
